e551abd581a69dde539371d7a235de55ca6940ce
Shruti Bhati <shrutibhati007@gmail.com>
2022-03-04 14:37:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {Routes,Route} from 'react-router-dom'
+import React from "react"
+import Header from './components/Header'
+function App(){
+    return (
+        
+          <Header/>    )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+const Header =() =>{
+    return <Header>
+        <button>Add New Expence</button>
+    </Header>
+}
+
+export default Header;
