fd1d3e5b4f38055614693f89c1919f8c3943602c
Shruti Bhati <shrutibhati007@gmail.com>
2022-03-22 21:03:31 +0530

    updated the new build

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,62 @@
+import { useState, useEffect, useContext } from 'react';
+import Expenses from "../Expenses/Expenses";
+import NewExpense from "../Expenses/NewExpense";
+import { UserContext } from '../../App';
+import { useNavigate } from 'react-router-dom';
+const Dashboard = () => {
+    const useremail = useContext(UserContext)[0]
+    const navigate = useNavigate()
+    //let [searchParams, setSearchParams] = useSearchParams();
+    const [expenses, setExpenses] = useState([]);
+
+    useEffect(() => {
+        if (!useremail || useremail === "") {
+            navigate('/Expenses-Tracker/login')
+        }
+        const data = localStorage.getItem(useremail)
+        if (!data) {
+            return
+        }
+        const jsonData = JSON.parse(data)
+        if (!jsonData.expenses) {
+            return
+        }
+        const newState = jsonData.expenses.map((value) => {
+            return {
+                id: value.id,
+                title: value.title,
+                amount: value.amount,
+                date: new Date(value.date),
+            }
+        })
+        setExpenses(newState)
+    }, [])
+
+    const addNewDataHandler = (expense) => {
+
+        // const useremail = searchParams.get("user")
+        const data = localStorage.getItem(useremail)
+        const jsonData = JSON.parse(data)
+        jsonData.expenses.push(expense);
+        localStorage.setItem(useremail, JSON.stringify(jsonData))
+        setExpenses(jsonData.expenses.map((value) => {
+            return {
+                id: value.id,
+                title: value.title,
+                amount: value.amount,
+                date: new Date(value.date),
+            }
+        }))
+    }
+
+    return (<>
+
+            <NewExpense addNewDataHandler={addNewDataHandler} />
+            <Expenses items={expenses} />
+
+    </>
+    )
+}
+
+
+export default Dashboard;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,112 @@
+import { useContext, useState} from "react";
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+const Login = () => {
+  const setCurrentUser = useContext(UserContext)[1]
+
+ 
+  const navigate = useNavigate()
+ const succefullyLogin = () => {
+   navigate('/Expenses-Tracker/dashboard');
+ }
+
+  
+  const classes = useStyles();
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  const emailHandler = (e) => {
+    setUserEmail(e.target.value)
+  }
+  const passwordHandler = (e) => {
+    setUserPassword(e.target.value)
+  }
+
+
+
+  const loginFormHandler = (e) => {
+    e.preventDefault();
+    const userData = JSON.parse(localStorage.getItem(userEmail))
+    if (userData !== null) {
+
+      if (userPassword === userData.password) {
+        succefullyLogin();
+        setCurrentUser(userEmail)
+
+
+      }
+      else {
+        setErrorMessage("Incorrect Password")
+      }
+
+
+    }
+    else {
+      setErrorMessage("Please create your account")
+    }
+
+
+    setUserEmail('');
+    setUserPassword('');
+
+  }
+  return (<>
+   
+  
+  <div>
+    <form className={classes.root} onSubmit={loginFormHandler}>
+
+
+      <TextField
+        label="Email"
+        variant="filled"
+        type="email"
+        required
+        value={userEmail}
+        onChange={emailHandler}
+      />
+      <TextField
+        label="Password"
+        variant="filled"
+        type="password"
+        required
+        value={userPassword}
+        onChange={passwordHandler}
+      />      <div>
+        <Button type="submit" variant="contained" color="primary" >
+          Login
+        </Button>
+      </div>
+      <div><span>{errorMessage}</span> </div>
+
+    </form >
+  </div >
+  </>)
+}
+export default Login;
